d2caff4e8bde53ab7e1a6c730a4c52fa27d04919
D_p <56536197+devaprasadnm@users.noreply.github.com>
2021-10-01 10:04:03 +0530

    Update calc.cpp

--- calc.cpp
+++ calc.cpp
@@ -1,50 +1,40 @@
 #include <iostream>
-#include <cstdlib>
+#include <fstream>
 using namespace std;
 
+class boys
+{
+    char name[20];
+    int age;
+    float ht;
+
+public:
+    void get()
+    {
+        cin >> name >> age >> ht;
+    }
+    void show()
+    {
+        cout << name << " " << age << " " << ht << "\n";
+    }
+};
 int main()
 {
-
-	double num1, num2, result;
-	int value;
-
-	cout << "Select the operations" << endl;
-	cout << "1. Addition \t\t 2. Subtraction \t\t 3. Division \t\t 4. Multiplication" << endl;
-	cin >> value;
-
-	cout << "Enter the two numbers" << endl;
-	cout << "num1: ";
-	cin >> num1;
-	cout << "num2: ";
-	cin >> num2;
-
-	switch(value)
-	{
-		case 1:
-			result = num1 + num2;
-			cout << "Result "<< result << endl;
-		  	break;
-
-		case 2:
-			result = num1 - num2;
-			cout << "Result "<< result << endl;
-		  	break;
-
-		case 3:
-			result = num1 / num2;
-			cout << "Result "<< result << endl;
-		  	break;
-
-		case 4:
-			result = num1 * num2;
-			cout << "Result "<< result << endl;
-		  	break;
-
-		default:
-			cout << "Error!! input" << endl;
-	}
-	  
-	 
-	system("pause");
-	return 0;
+    boys b[3], a[3];
+    fstream ft;
+    ft.open("boys.txt", ios::in | ios::out);
+    for (int i = 0; i < 3; i++)
+    {
+        b[i].get();
+        ft.write((char *)&b[i], sizeof(b[i]));
+    }
+    ft.seekg(0);
+    cout << "Entered values are";
+    for (int i = 0; i < 3; i++)
+    {
+        ft.read((char *)&a[i], sizeof(b[i]));
+        a[i].show();
+    }
+    ft.close();
+    return 0;
 }
